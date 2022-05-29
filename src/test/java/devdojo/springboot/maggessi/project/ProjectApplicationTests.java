c3200ad539737341af5b59ad2c3dc84b1af679d1
Bernardo Maggessi <bermaggessi2@yahoo.com>
2022-05-29 18:35:09 -0300

    first commit

---
+++ src/test/java/devdojo/springboot/maggessi/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package devdojo.springboot.maggessi.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
